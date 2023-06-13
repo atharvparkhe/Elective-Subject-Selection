88b61bca7289224c5b61657fa2845dc952288a3b
Abhinav <abhinavborde.mail@gmail.com>
2023-06-13 23:12:03 +0530

    mail msg added

--- core/settings.py
+++ core/settings.py
@@ -164,13 +164,13 @@ CACHES = {
 # SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
 # Email Service Setup
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'pop.greeshmamedam.tech'
+EMAIL_PORT = 110
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+
 
 # Utils
 BASE_URL = "http://127.0.0.1:8000/"
