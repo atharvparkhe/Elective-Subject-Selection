5d93d6c43c70f9213299fcaa86950296750b8c41
Atharva Parkhe <patharv777@gmail.com>
2023-06-23 00:21:21 +0530

    cors allow all origin

--- core/settings.py
+++ core/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'storages',
     'base.apps.BaseConfig',
@@ -36,6 +37,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -155,6 +157,8 @@ CACHES = {
         }
     }
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 # __production
 # SESSION_COOKIE_SECURE = True
